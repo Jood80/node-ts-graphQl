c6906224ac853547e615e324593ddbe7213f0758
Nujood <shawar.nujood@gmail.com>
2021-05-11 20:19:54 +0300

    Set up the types of user Entity

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,48 @@ import 'reflect-metadata';
 import { GraphQLServer } from 'graphql-yoga';
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
+import { ResolverMap } from './types/ResolverTypes';
 
 const typeDefs = `
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    age: int!
+    email: String!
+    isConfirmed: boolean!
+  }
+
   type Query {
     hello(name:String): String!
+    user(id: Int!): User!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(firstName: String!, lastName: String!, age: Int!, email: String!, isConfirmed: Boolean!): User!
+    updateUser(firstName: String!, lastName: String!, age: Int!, email: String!, isConfirmed: Boolean!): Boolean
+    deleteUser(id: Int!): Boolean
+
   }
 `;
 
-const resolvers = {
+const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    user: (parent, args, context, info) => ({}),
   },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 createConnection()
-  .then(async(connection) => {
+  .then(async (connection) => {
     console.log('Inserting a new user into the database...');
     const user = new User();
-    user.firstName = 'John';
-    user.lastName = 'Snow';
-    user.age = 25;
-    user.email = 'test4@gmail.com';
+    user.firstName = 'Arya';
+    user.lastName = 'Stark';
+    user.age = 16;
+    user.email = 'test5@gmail.com';
     user.isConfirmed = true;
     await connection.manager.save(user);
     console.log('Saved a new user with id: ' + user.id);
