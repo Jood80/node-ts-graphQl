272f426800e1bb890593ad3a8726683ac8634474
Nujood <shawar.nujood@gmail.com>
2021-05-18 23:58:21 +0300

    Make a custome express server using apollo graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import 'reflect-metadata';
 import * as path from 'path';
-import { GraphQLServer } from 'graphql-yoga';
+import express from 'express';
+// import expressPlayground from 'graphql-playground-middleware-express';
+const {
+  graphqlExpress,
+  makeExecutableSchema,
+} = require('apollo-server-express');
+
 import { createConnection } from 'typeorm';
 import { importSchema } from 'graphql-import';
 import * as argon2 from 'argon2';
 import * as jwt from 'jsonwebtoken';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 import { ResolverMap } from './types/ResolverTypes';
 import { User } from './entity/User';
 import { Profile } from './entity/Profile';
 import { GQL } from './generated/types';
 
-
 const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
-dotenv.config()
+dotenv.config();
 
 const resolvers: ResolverMap = {
   Query: {
@@ -59,7 +66,11 @@ const resolvers: ResolverMap = {
       return true;
     },
 
-    registerUser: async (_, args: GQL.IRegisterUserOnMutationArguments, { res }) => {
+    registerUser: async (
+      _,
+      args: GQL.IRegisterUserOnMutationArguments,
+      { res },
+    ) => {
       try {
         const password = await argon2.hash(args.password);
         const user = User.create({
@@ -89,9 +100,40 @@ const resolvers: ResolverMap = {
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const app = express();
+
+const options = {
+  port: process.env.PORT || 4000,
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  options,
+});
+
+app.cors({
+  credential: true,
+  origin: process.env.DEVELOPMENT_HOST,
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  '/graphql',
+  express.json(),
+  graphqlExpress((_: any, res: any) => ({
+    schema,
+    context: { res },
+  })),
+);
+
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+app.listen(process.env.PORT || 3000)
+
 createConnection()
   .then(() => {
-    server.start(() => console.log('Server is running on localhost:4000'));
+    app.listen(() => console.log('Server is running on localhost:4000'));
   })
   .catch((error) => console.log(error));
